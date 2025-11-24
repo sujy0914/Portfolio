7d55a3984ce73a3a1ac22c7371ee2d9e9d3f3722
sujy0914 <sujy0914@naver.com>
2025-11-24 13:37:37 +0900

    진짜최최종

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/theme.css'
-import App from './app.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
